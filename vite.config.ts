05f4256e1e9875223b18524206b7603c915e5e99
NABANNA CHOUDHURY <165921831+nabanna12@users.noreply.github.com>
2026-05-11 22:20:02 +0530

    Remove manualChunks configuration from vite.config
    Removed manual chunking configuration from build options.

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          three:  ['three'],
-          motion: ['framer-motion'],
-          vendor: ['react', 'react-dom'],
-        },
-      },
-    },
-  },
 })
